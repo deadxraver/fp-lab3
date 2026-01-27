486ec9009c5f0824a2739c7d8f9bcd2b2f5287bc
deadxraver <409853@edu.itmo.ru>
2026-01-27 23:25:33 +0300

    argparser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main (main) where
 
+import System.Environment
+
 import Lib
 
-main :: IO ()
-main = someFunc
+check :: String -> [String] -> Bool
+check y (x : xs) = (x == y) || check y xs
+check _ [] = False
+
+checkLinear :: [String] -> Bool
+checkLinear = check "--linear"
+
+checkLagrange :: [String] -> Bool
+checkLagrange = check "--lagrange"
+
+main = do
+    args <- getArgs
+    let linear = checkLinear args
+        lagrange = checkLagrange args
+     in putStrLn $ "Linear:" ++ show linear ++ "\nLagrange:" ++ show lagrange
