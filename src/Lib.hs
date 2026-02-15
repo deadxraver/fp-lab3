68c6c5fddd4d8f9ac41710e7b3a004d8051e7c44
deadxraver <409853@edu.itmo.ru>
2026-02-15 15:45:57 +0300

    lagrange function ready

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,8 +3,14 @@ module Lib (
     lagrangeInterpolate,
 ) where
 
-linearInterpolate :: [(Float, Float)] -> Float
-linearInterpolate _ = 0 -- TODO:
+linearInterpolate :: Double -> [(Double, Double)] -> Double
+linearInterpolate _ _ = 0 -- TODO:
 
-lagrangeInterpolate :: [(Float, Float)] -> Float
-lagrangeInterpolate _ = 0 -- TODO:
+--                     x          X           Y           res
+lagrangeInterpolate :: Double -> [Double] -> [Double] -> Double
+lagrangeInterpolate _ _ [] = 0
+lagrangeInterpolate x xarr (y1 : yarr) = y1 * lagrangeInterpolate' xarr + lagrangeInterpolate x xarr yarr
+  where
+    x1i = xarr !! (length xarr - length yarr - 1)
+    lagrangeInterpolate' [] = 1
+    lagrangeInterpolate' (x1' : xarr') = (if x1' == x1i then 1 else (x - x1') / (x1i - x1')) * lagrangeInterpolate' xarr'
