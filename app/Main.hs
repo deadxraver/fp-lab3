326343f559c80b20c1802df922a43040553baa97
deadxraver <409853@edu.itmo.ru>
2026-02-16 00:14:33 +0300

    new interaction method

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import Control.Monad
-import Data.Maybe (mapMaybe)
+import Data.Maybe (fromJust, mapMaybe)
 import System.Environment
 import System.IO
 
@@ -27,7 +27,7 @@ getStep ("--step" : step : _) = Just $ read step
 getStep (_ : ss) = getStep ss
 getStep [] = Nothing
 
-main = do
+{-main = do
     args <- getArgs
     let linear = checkLinear args
         lagrange = checkLagrange args
@@ -50,3 +50,40 @@ main = do
             if not (linear || lagrange)
                 then error "You must pick at least 1 method (linear/lagrange)"
                 else interact process
+                  -}
+
+main = do
+    args <- getArgs
+    let linear = checkLinear args
+        lagrange = checkLagrange args
+        maybeN = getN args
+        maybeStep = getStep args
+        parsePair line = case map (read :: String -> Float) (words line) of
+            [x, y] -> do
+                Just (x, y)
+            _ -> Nothing
+        loop x xarr yarr = do
+            eof <- isEOF
+            if eof
+                then return ()
+                else do
+                    line <- getLine
+                    case parsePair line of
+                        Just (xi, yi) -> do
+                            let newXarr = xi : xarr
+                                newYarr = yi : yarr
+                                step = fromJust maybeStep
+                            -- TODO:
+                            putStrLn $ "x=" ++ show x ++ ",X=" ++ show newXarr ++ ",Y=" ++ show newYarr
+                            loop (x + step) newXarr newXarr
+                        Nothing -> error $ "unknown format:" ++ line
+    case maybeStep of
+        Just step -> putStrLn $ "step=" ++ show step
+        Nothing -> error "step not specified"
+    when lagrange $
+        case maybeN of
+            Just n -> putStrLn $ "n=" ++ show n
+            Nothing -> error "n not specified for Lagrange"
+    unless (lagrange || linear) $
+        error "at least 1 method should be picked"
+    loop 0 [] []
